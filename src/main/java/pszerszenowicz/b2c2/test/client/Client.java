4f680a2c9d865f2b06c4c993aeb7e2ea5e09d092
Przemek <przemyslaw.szerszenowicz@gmail.com>
2024-11-22 03:35:48 +0100

    1st working version

--- src/main/java/pszerszenowicz/b2c2/test/client/Client.java
+++ src/main/java/pszerszenowicz/b2c2/test/client/Client.java
@@ -1,81 +1,86 @@
 package pszerszenowicz.b2c2.test.client;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.DefaultHttpHeaders;
 import io.netty.handler.codec.http.HttpClientCodec;
-import io.netty.handler.codec.http.websocketx.WebSocketClientHandshakerFactory;
-import io.netty.handler.codec.http.websocketx.WebSocketClientProtocolHandler;
-import io.netty.handler.codec.http.websocketx.WebSocketVersion;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketClientCompressionHandler;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URI;
 
 public class Client {
     private final String targetHost = "localhost";
     private final int targetPort = 8080;
 
-    private ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
     private Channel channel;
 
-    public void start() throws InterruptedException {
+    public void start() throws Exception {
+        URI uri = new URI("ws://"+targetHost+":"+targetPort+"/");
         EventLoopGroup group = new NioEventLoopGroup();
         try {
-            Bootstrap bootstrap = new Bootstrap();
-            bootstrap.group(group)
+            // Connect with V13 (RFC 6455 aka HyBi-17). You can change it to V08 or V00.
+            // If you change it to V00, ping is not supported and remember to change
+            // HttpResponseDecoder to WebSocketHttpResponseDecoder in the pipeline.
+            final WebSocketClientHandler handler =
+                    new WebSocketClientHandler(
+                            WebSocketClientHandshakerFactory.newHandshaker(
+                                    uri, WebSocketVersion.V13, null, true, new DefaultHttpHeaders()));
+
+            Bootstrap b = new Bootstrap();
+            b.group(group)
                     .channel(NioSocketChannel.class)
                     .handler(new ChannelInitializer<SocketChannel>() {
-
                         @Override
-                        protected void initChannel(SocketChannel socketChannel) throws Exception {
-                            socketChannel.pipeline().addLast(
+                        protected void initChannel(SocketChannel ch) {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(
                                     new LoggingHandler(LogLevel.INFO),
                                     new HttpClientCodec(),
-                                    new WebSocketClientProtocolHandler(
-                                            WebSocketClientHandshakerFactory.newHandshaker(
-                                                    new URI("ws://" + targetHost + ":" + targetPort), WebSocketVersion.V13, null, false, new DefaultHttpHeaders())
-                                    ),
-                                    new ClientResponseHandler());
+                                    new HttpObjectAggregator(8192),
+                                    WebSocketClientCompressionHandler.INSTANCE,
+                                    handler);
                         }
                     });
-            ChannelFuture future = bootstrap.connect(targetHost, targetPort).sync();
-            channel = future.channel();
-            future.channel().closeFuture().addListener(new ChannelFutureListener() {
-                @Override
-                public void operationComplete(ChannelFuture channelFuture) throws Exception {
-                    group.shutdownGracefully();
+
+            Channel ch = b.connect(targetHost, targetPort).sync().channel();
+            handler.handshakeFuture().sync();
+
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                String msg = console.readLine();
+                if (msg == null) {
+                    break;
+                } else if ("bye".equals(msg.toLowerCase())) {
+                    ch.writeAndFlush(new CloseWebSocketFrame());
+                    ch.closeFuture().sync();
+                    break;
+                } else if ("ping".equals(msg.toLowerCase())) {
+                    WebSocketFrame frame = new PingWebSocketFrame(Unpooled.wrappedBuffer(new byte[] { 8, 1, 8, 1 }));
+                    ch.writeAndFlush(frame);
+                } else {
+                    WebSocketFrame frame = new TextWebSocketFrame(msg);
+                    ch.writeAndFlush(frame);
                 }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
+            }
+        } finally {
             group.shutdownGracefully();
         }
     }
 
-    public void sendMessage(String message) {
-        if(channel != null && channel.isActive()) {
-            channel.writeAndFlush(message);
-        }
-    }
 
-    private void closeChannel(){
-        if(channel != null && channel.isActive()) {
-            channel.close();
-        }
-    }
-
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         Client client = new Client();
         client.start();
-
-        client.sendMessage("test");
-
 
     }
 
